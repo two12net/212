ae45c396329b381e6bac83571321e0eb5669ab01
Andreas Egerer <info@two12.net>
2013-03-26 15:41:02 +0100

    New flat layout

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+/*
+* some js functions
+*/
+
+/* menu 
+$(document).ready(function (){
+	$("#menuProjects").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#contentProjects").offset().top
+		}, 2000);
+	});
+});
+*/
